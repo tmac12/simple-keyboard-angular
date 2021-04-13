7a905f19f86d8d7ba212edd144b9653d4173ac60
tmac12 <mba121@gmail.com>
2021-04-13 18:41:11 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,94 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from "@angular/core";
+import Keyboard from "simple-keyboard";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  encapsulation: ViewEncapsulation.None,
+  templateUrl: "./app.component.html",
+  styleUrls: [
+    "../../node_modules/simple-keyboard/build/css/index.css",
+    "./app.component.scss"
+  ]
 })
 export class AppComponent {
-  title = 'keyboar';
+  keyboard: Keyboard;
+  inputName = "firstName";
+  inputs = {
+    firstName: "Boaty",
+    lastName: "McBoatface"
+  };
+
+  ngAfterViewInit() {
+    this.keyboard = new Keyboard({
+      debug: true,
+      inputName: this.inputName,
+      onChange: (input) => this.onChange(input),
+      onKeyPress: (button) => this.onKeyPress(button),
+      preventMouseDownDefault: true // If you want to keep focus on input
+    });
+
+    /**
+     * Since we have default values for our inputs,
+     * we must sync them with simple-keyboard
+     */
+    this.keyboard.replaceInput(this.inputs);
+  }
+
+  onInputFocus = (event: any) => {
+    this.inputName = event.target.id;
+
+    console.log("Focused input", this.inputName);
+
+    this.keyboard.setOptions({
+      inputName: event.target.id
+    });
+  };
+
+  setInputCaretPosition = (elem: any, pos: number) => {
+    if (elem.setSelectionRange) {
+      elem.focus();
+      elem.setSelectionRange(pos, pos);
+    }
+  };
+
+  onInputChange = (event: any) => {
+    this.keyboard.setInput(event.target.value, event.target.id);
+  };
+
+  onChange = (input: string) => {
+    this.inputs[this.inputName] = input;
+    console.log("Input changed", input);
+
+    /**
+     * Synchronizing input caret position
+     * This part is optional and only relevant if using the option "preventMouseDownDefault: true"
+     */
+    let caretPosition = this.keyboard.caretPosition;
+
+    if (caretPosition !== null)
+      this.setInputCaretPosition(
+        document.querySelector(`#${this.inputName}`),
+        caretPosition
+      );
+
+    console.log("caretPosition", caretPosition);
+  };
+
+  onKeyPress = (button: string) => {
+    console.log("Button pressed", button);
+
+    /**
+     * If you want to handle the shift and caps lock buttons
+     */
+    if (button === "{shift}" || button === "{lock}") this.handleShift();
+  };
+
+  handleShift = () => {
+    let currentLayout = this.keyboard.options.layoutName;
+    let shiftToggle = currentLayout === "default" ? "shift" : "default";
+
+    this.keyboard.setOptions({
+      layoutName: shiftToggle
+    });
+  };
 }
